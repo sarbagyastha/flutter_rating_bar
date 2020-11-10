c05f860b5ea47fc328392f2548a8e1d3363d782f
sarbagyastha <sarbagyastha@gmail.com>
2020-11-10 09:03:36 +0545

    broke down into multiple files

---
+++ lib/src/rating_bar_indicator.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+
+/// A widget to display rating as assigned using [rating] property.
+///
+/// It's a read only version of [RatingBar].
+/// Use [RatingBar], if interative version is required. i.e. if user input is required.
+class RatingBarIndicator extends StatefulWidget {
+  /// Defines the rating value for indicator.
+  ///
+  /// Default = 0.0
+  final double rating;
+
+  /// {@macro flutterRatingBar.itemCount}
+  final int itemCount;
+
+  /// {@macro flutterRatingBar.itemSize}
+  final double itemSize;
+
+  /// {@macro flutterRatingBar.itemPadding}
+  final EdgeInsets itemPadding;
+
+  /// Controls the scrolling behaviour of rating bar.
+  ///
+  /// Default is [NeverScrollableScrollPhysics].
+  final ScrollPhysics physics;
+
+  /// {@macro flutterRatingBar.textDirection}
+  final TextDirection textDirection;
+
+  /// {@macro flutterRatingBar.itemBuilder}
+  final IndexedWidgetBuilder itemBuilder;
+
+  /// {@macro flutterRatingBar.direction}
+  final Axis direction;
+
+  /// {@macro flutterRatingBar.unratedColor}
+  final Color unratedColor;
+
+  RatingBarIndicator({
+    @required this.itemBuilder,
+    this.rating = 0.0,
+    this.itemCount = 5,
+    this.itemSize = 40.0,
+    this.itemPadding = const EdgeInsets.all(0.0),
+    this.physics = const NeverScrollableScrollPhysics(),
+    this.textDirection,
+    this.direction = Axis.horizontal,
+    this.unratedColor,
+  });
+
+  @override
+  _RatingBarIndicatorState createState() => _RatingBarIndicatorState();
+}
+
+class _RatingBarIndicatorState extends State<RatingBarIndicator> {
+  double _ratingFraction = 0.0;
+  int _ratingNumber = 0;
+  bool _isRTL = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _ratingNumber = widget.rating.truncate() + 1;
+    _ratingFraction = widget.rating - _ratingNumber + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _isRTL = (widget.textDirection ?? Directionality.of(context)) ==
+        TextDirection.rtl;
+    _ratingNumber = widget.rating.truncate() + 1;
+    _ratingFraction = widget.rating - _ratingNumber + 1;
+    return SingleChildScrollView(
+      scrollDirection: widget.direction,
+      physics: widget.physics,
+      child: widget.direction == Axis.horizontal
+          ? Row(
+              mainAxisSize: MainAxisSize.min,
+              textDirection: _isRTL ? TextDirection.rtl : TextDirection.ltr,
+              children: _children(),
+            )
+          : Column(
+              mainAxisSize: MainAxisSize.min,
+              textDirection: _isRTL ? TextDirection.rtl : TextDirection.ltr,
+              children: _children(),
+            ),
+    );
+  }
+
+  List<Widget> _children() {
+    return List.generate(
+      widget.itemCount,
+      (index) {
+        if (widget.textDirection != null) {
+          if (widget.textDirection == TextDirection.rtl &&
+              Directionality.of(context) != TextDirection.rtl) {
+            return Transform(
+              transform: Matrix4.identity()..scale(-1.0, 1.0, 1.0),
+              alignment: Alignment.center,
+              transformHitTests: false,
+              child: _buildItems(index),
+            );
+          }
+        }
+        return _buildItems(index);
+      },
+    );
+  }
+
+  Widget _buildItems(int index) => Padding(
+        padding: widget.itemPadding,
+        child: SizedBox(
+          width: widget.itemSize,
+          height: widget.itemSize,
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              FittedBox(
+                fit: BoxFit.contain,
+                child: index + 1 < _ratingNumber
+                    ? widget.itemBuilder(context, index)
+                    : ColorFiltered(
+                        colorFilter: ColorFilter.mode(
+                          widget.unratedColor ?? Colors.grey[200],
+                          BlendMode.srcIn,
+                        ),
+                        child: widget.itemBuilder(context, index),
+                      ),
+              ),
+              if (index + 1 == _ratingNumber)
+                _isRTL
+                    ? FittedBox(
+                        fit: BoxFit.contain,
+                        child: ClipRect(
+                          clipper: _IndicatorClipper(
+                            ratingFraction: _ratingFraction,
+                            rtlMode: _isRTL,
+                          ),
+                          child: widget.itemBuilder(context, index),
+                        ),
+                      )
+                    : FittedBox(
+                        fit: BoxFit.contain,
+                        child: ClipRect(
+                          clipper: _IndicatorClipper(
+                            ratingFraction: _ratingFraction,
+                          ),
+                          child: widget.itemBuilder(context, index),
+                        ),
+                      ),
+            ],
+          ),
+        ),
+      );
+}
+
+class _IndicatorClipper extends CustomClipper<Rect> {
+  final double ratingFraction;
+  final bool rtlMode;
+
+  _IndicatorClipper({
+    this.ratingFraction,
+    this.rtlMode = false,
+  });
+
+  @override
+  Rect getClip(Size size) => rtlMode
+      ? Rect.fromLTRB(
+          size.width - size.width * ratingFraction,
+          0.0,
+          size.width,
+          size.height,
+        )
+      : Rect.fromLTRB(
+          0.0,
+          0.0,
+          size.width * ratingFraction,
+          size.height,
+        );
+
+  @override
+  bool shouldReclip(CustomClipper<Rect> oldClipper) => true;
+}
